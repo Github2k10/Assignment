a1d5dc4a15859b1182e397de63c1fcfc516289d1
Github2k10 <ankitsingh40068@gmail.com>
2023-09-05 21:00:54 +0530

    Assignment update

--- Assignment_3/Problem3.java
+++ Assignment_3/Problem3.java
@@ -7,11 +7,15 @@ public class Problem3 {
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
 
-        if(num % 2 == 0 && num > 10)
-            System.out.println("The number is even and greater than 10.");
+        if(num % 2 == 0)
+            if(num > 10)
+                System.out.println("The number is even and greater than 10.");
+            else
+                System.out.println("The number is not greater than 10.");
         else 
-            System.out.println("The number is neither even nor greater than 10.");
-
+            System.out.println("The number is not even.");
+    
+        
         sc.close();
     }
 }
